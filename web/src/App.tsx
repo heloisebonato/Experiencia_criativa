0cf24d47827ffc8e180fc6c8adc80d37040cfcd3
Guipb10 <gui.pb1@hotmail.com>
2021-04-12 22:44:07 -0300

    imagem-logo
    coloquei a img e fonte

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoinicio from './img/logo-inicio.jpeg';
+import nuveminicio from './img/nuvem2.jpeg';
 
 
 function App() {
   return (
-    <div className="App">
-      <img src="../src/img/nuvem1.jpeg"></img>
+    <div id="page-landing">
+        <div className="sec01">
+          <img className="logoinicio" src={logoinicio} alt="logo" />
+
+          <main className="text01">
+            <p>Adicionar um resump sobre o que é a safe city</p>
+          </main>
+        </div>
     </div>
   );
 }
